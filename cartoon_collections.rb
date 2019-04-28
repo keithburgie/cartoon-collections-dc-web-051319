55287c30bda1638c343db9c41fbeac8705181561
Keith Burgie <krburgie@gmail.com>
2019-04-28 01:44:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese(menu)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   menu.find do |food|
-    food.include?(cheese_types[food])
+    food.include?(cheese_types)
   end
 end
-find_the_cheese(["tomato soup", "gouda", "oyster crackers", "gouda"])
+find_the_cheese(["tomato soup", "cheddar", "oyster crackers", "gouda"])
